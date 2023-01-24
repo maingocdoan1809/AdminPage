ce2816e87c3a037e364e4282bb5fae42c3793414
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">
+    <h3>Đoàn đẹp trai nè:)))</h3>
+  </div>;
+}
+
+export default App;
