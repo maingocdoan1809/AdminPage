e5d0e5fd45ea325469717425359f7a09d65209de
Mai Ngoc Doan <maidoan2002ns@gmail.com>
2023-04-14 02:29:19 +0000

    Update App.tsx: H3

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">
-    <h3>Đoàn đẹp trai nè:)))</h3>
-  </div>;
+  return (
+    <div className="App">
+      <h3>Đoàn đẹp trai nè:))) dfdfdf</h3>
+    </div>
+  );
 }
 
 export default App;
